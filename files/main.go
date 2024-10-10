c5cf2fffe0941418a37b8b8f07b3cb9e57e6e97c
823Njoroge <njorogeian823@gmail.com>
2024-10-10 12:44:28 +0300

    files

--- files/main.go
+++ files/main.go
@@ -1,4 +1,36 @@
 package main
-func main(){
 
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	fmt.Println("Welcome to files in golang")
+	content:="Add this to a file"
+
+	file, err:=os.Create("./myfile.txt")
+
+	if err !=nil{
+		panic(err)
+	}
+length, err:=io.WriteString(file,content)
+if err !=nil{
+	panic(err)
+}
+fmt.Println("Length is:",length)
+ defer file.Close()
+ readFile("./myfile.txt")
+} 
+
+
+
+func readFile(filname string){
+	databyte, err:=ioutil.ReadFile(filname)
+	if err !=nil{
+		panic(err)
+	}
+	fmt.Println("The data in the file is \n",string(databyte))
 }

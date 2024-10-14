ca87cf2278084962fa0010c9d23b6f19fc365086
823Njoroge <njorogeian823@gmail.com>
2024-10-14 23:12:22 +0300

    post json req

--- webverb/main.go
+++ webverb/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func main() {
@@ -11,7 +13,7 @@ func main() {
 }
 
 func PerformGetRequest(){
-	const myurl="http://localhost:5500/get"
+	const myurl="http://localhost:8000/get"
 	response,err:=http.Get(myurl)
 	if err!=nil{
 		panic(err)
@@ -19,4 +21,25 @@ func PerformGetRequest(){
 	
 	defer response.Body.Close()
 	fmt.Println("The response is:",response.StatusCode)
+}
+func PerformPostJson(){
+	const myurl="http://localhost:8000/post"
+
+	//fake json data
+	requestBody:=strings.NewReader(`
+	{
+	"coursename":"Golang"
+	"price":"$20"
+	"platform":"youtube"
+	
+	}
+	
+	`)
+	response,err:=http.Post(myurl,"application/json",requestBody)
+	if err!=nil{
+		panic(err)
+	}
+	defer response.Body.Close()
+	content,_:=ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
 }

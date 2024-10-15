fb08f510e3b6fab6e4f565c7b454d0adc390dd5f
823Njoroge <njorogeian823@gmail.com>
2024-10-15 23:14:08 +0300

    post form req

--- webverb/main.go
+++ webverb/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func main() {
 	fmt.Println("Hello, lets learn web requests")
-	PerformGetRequest()
+	//PerformGetRequest()
+	PerformPostFormRequest()
 }
 
 func PerformGetRequest(){
@@ -42,4 +44,23 @@ func PerformPostJson(){
 	defer response.Body.Close()
 	content,_:=ioutil.ReadAll(response.Body)
 	fmt.Println(string(content))
+}
+
+func PerformPostJsonRequest(){
+
+}
+func PerformPostFormRequest(){
+	const myurl="http://localhost:8000/postform"
+	data:=url.Values{}
+	data.Add("firstname","Ianoh")
+	data.Add("lastname","ke")
+	data.Add("course","IT")
+	
+	response,err:=http.PostForm(myurl,data)
+	if err!=nil{
+		panic(err)
+	}
+	defer response.Body.Close()
+	content,_:=ioutil.ReadAll(response.Body)
+	fmt.Println(string(content) )
 }

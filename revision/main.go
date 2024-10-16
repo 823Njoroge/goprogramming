a4fdf95769c5f437af0e889748ae7c06a2ecaca7
823Njoroge <njorogeian823@gmail.com>
2024-10-16 22:58:46 +0300

    34

---
+++ revision/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+func getPerimeter(width,length int){
+	perimeter:=width*2 + length*2
+	fmt.Print("The perimeter is:",perimeter)
+	
+}
+//first challenge
+func main() {
+	width := 24
+	length := 23
+	
+		area:= length *width
+		fmt.Print("The area of the rectangle is",area)
+
+	
+
+
+}

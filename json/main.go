33ce4acefa19499b58c634314cd1586b24921a37
823Njoroge <njorogeian823@gmail.com>
2024-10-18 23:18:59 +0300

    json

--- json/main.go
+++ json/main.go
@@ -5,29 +5,34 @@ import (
 	"fmt"
 )
 
-type epl struct {
-	name     string
-	trophies int
-	players  int
+type EPL struct {
+	Name     string `json:"name"`
+	Trophies int    `json:"trophies"`
+	Players  int    `json:"players"`
 }
 
 func main() {
-	fmt.Println("json code")
-	encodeJson()
+	fmt.Println("JSON encoding in Go")
+	encodeJSON()
 }
 
-func encodeJson(){
-	league:=[]epl{
-		{"Chelsea",23,56},
-		{"Manjesta",12,67},
-		{"Liverpool",9,25},
-		{"Arsenal",12,45},
+func encodeJSON() {
+	league := []EPL{
+		{"Chelsea", 23, 56},
+		{"Manjesta", 12, 67},
+		{"Liverpool", 9, 25},
+		{"Arsenal", 12, 45},
 	}
-fmt.Print(league)
-finalJson,err:=json.MarshalIndent(league,"","\t")
-if err!=nil{
-	panic(err)
 
-}
-fmt.Printf("%s\n",finalJson)
+	// Printing the struct array for reference
+	fmt.Println("Original Struct Data:", league)
+
+	// Encoding to JSON
+	finalJSON, err := json.MarshalIndent(league, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+
+	// Printing the encoded JSON
+	fmt.Printf("Encoded JSON:\n%s\n", finalJSON)
 }

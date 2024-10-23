e2b63cc8205b82638af11ffb12ce076576790e20
823Njoroge <njorogeian823@gmail.com>
2024-10-23 21:58:27 +0300

    sending Api-json response

--- buildapi/main.go
+++ buildapi/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type Course struct {
 	CourseId    string  `json:"courseid"`
 	CourseName  string  `json:"coursename"`
@@ -20,5 +26,19 @@ func (c *Course) IsEmpty() bool {
 	return c.CourseId == "" && c.CourseName == ""
 }
 func main() {
+
+}
+
+//controllers- they go to a different file
+
+// serve home route
+func serveHome(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("<h1>Hi! My name is Ian and I'm learning about APIs"))
+}
+
+func getAllCourses(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Get all courses")
+	w.Header().Set("Content-Type","application/json")
+	json.NewEncoder(w).Encode(courses)
 
 }

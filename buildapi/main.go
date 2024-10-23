d355705dcedf4a7250283f8a1b8f4b9db13aeb9b
823Njoroge <njorogeian823@gmail.com>
2024-10-23 21:25:49 +0300

    api

--- buildapi/main.go
+++ buildapi/main.go
@@ -1,7 +1,24 @@
-package make
+package main
 
-import "fmt"
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice string  `json:"courseprice"`
+	Author      *Author `json:"author"`
+}
 
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+// FAKE DATABASE
+var courses []Course
+
+// func malware
+func (c *Course) IsEmpty() bool {
+	return c.CourseId == "" && c.CourseName == ""
+}
 func main() {
-	fmt.Println("Gothaa")
+
 }
